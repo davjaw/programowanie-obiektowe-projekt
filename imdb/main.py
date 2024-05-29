b45486a0daa63dbe93058b4d4fd8138775b05c7f
Dawid Jaworski <dawidjaworski53@gmail.com>
2024-05-29 18:01:06 +0200

    Add automated click on load more button

--- imdb/main.py
+++ imdb/main.py
@@ -10,12 +10,16 @@ from selenium.webdriver.chrome.options import Options
 
 
 def main():
-    # url = "https://www.imdb.com/search/title/?title_type=feature&release_date=2023-01-01,2023-12-31"
+    SCROLL_PAUSE_TIME = 2
+    moreButtonExists = True
+
+    url = "https://www.imdb.com/search/title/?title_type=feature&release_date=2023-01-01,2023-12-31"
     testurl = "https://www.imdb.com/search/title/?title_type=feature&release_date=1988-01-01,1988-01-09"
     options = Options()
     options.headless = True
 
     driver = webdriver.Chrome(options=options)
+    # driver.get(url)
     driver.get(testurl)
 
     wait = WebDriverWait(driver, 10)
@@ -23,7 +27,24 @@ def main():
         (By.XPATH, ("//*[@data-testid='reject-button']"))))
     closeCookies.click()
 
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    scrollPage = driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);")
+
+    moreButtonClass = "//button[@class='ipc-btn ipc-btn--single-padding ipc-btn--center-align-content ipc-btn--default-height ipc-btn--core-base ipc-btn--theme-base ipc-btn--on-accent2 ipc-text-button ipc-see-more__button']"
+    moreButtonWait = wait.until(EC.presence_of_element_located(
+        (By.XPATH, moreButtonClass)))
+
+    while True:
+        scrollPage
+        moreButtonWait
+        driver.execute_script("arguments[0].click();", moreButtonWait)
+        moreButtonWait
+        try:
+            driver.find_element(
+                By.XPATH, "//span[contains(@class, 'ipc-see-more')]/button[contains(@class, 'ipc-btn--single-padding')]")
+        except:
+            print("End of page")
+            break
 
 
 if __name__ == "__main__":
