a294a5c537f313a0c9f73c8de1fb92687f3af8f6
Xaroo <pbujas0@wp.pl>
2024-06-05 23:29:56 +0200

    Added first chart

--- view/main.py
+++ view/main.py
@@ -1,31 +1,39 @@
 import tkinter
+import sys
 from customtkinter import *
 
-root = CTk()
+sys.path.append("..")
 
+from controller.main import plot_chart
+
+root = CTk()
 root.title("Python project")
 root.geometry("1000x1000")
 root.configure(fg_color="#1b243a")
 root.resizable(width=False, height=False)
 
+frame = CTkFrame(root, fg_color="#1b243a")
+frame.grid(row=3, column=0, columnspan=6, rowspan=6, padx=20, pady=20, sticky="nsew")
+
 pie1 = CTkCheckBox(root, text="% Ocen w przedziale gatunków")
 pie2 = CTkCheckBox(root, text="% Ocen w przedziale lat")
 pie3 = CTkCheckBox(root, text="% Gatunków w przedziale lat")
 bar1 = CTkCheckBox(root, text="Średnia ocen na każdy gatunek")
-bar2 = CTkCheckBox(root, text="Srednia ocen na każdy rok")
-point1= CTkCheckBox(root, text="Ilość wszystkich opinii na kazdy rok")
+bar2 = CTkCheckBox(root, text="Średnia ocen na każdy rok", command=lambda: plot_chart(frame, bar2))
+point1 = CTkCheckBox(root, text="Ilość wszystkich opinii na kazdy rok")
 point2 = CTkCheckBox(root, text="Rozkład procentowy gatunków na kazdy rok")
 
 def piecharts():
-    pie1.grid(row=2, column=0,padx=(120, 0), columnspan=2, sticky="w")
-    pie2.grid(row=2, column=2,padx=(40, 0), columnspan=2, sticky="w")
+    pie1.grid(row=2, column=0, padx=(120, 0), columnspan=2, sticky="w")
+    pie2.grid(row=2, column=2, padx=(40, 0), columnspan=2, sticky="w")
     pie3.grid(row=2, column=4, columnspan=2, sticky="w")
     bar1.grid_remove()
     bar2.grid_remove()
     point1.grid_remove()
     point2.grid_remove()
+
 def barcharts():
-    bar1.grid(row=2, column=0, padx=(120, 0), columnspan=3,sticky="w")
+    bar1.grid(row=2, column=0, padx=(120, 0), columnspan=3, sticky="w")
     bar2.grid(row=2, column=3, padx=(100, 0), columnspan=3, sticky="w")
     pie1.grid_remove()
     pie2.grid_remove()
@@ -35,7 +43,7 @@ def barcharts():
 
 def pointcharts():
     point1.grid(row=2, column=0, padx=(120, 0), columnspan=3, sticky="w")
-    point2.grid(row=2, column=3, padx=(100, 0),columnspan=3, sticky="w")
+    point2.grid(row=2, column=3, padx=(100, 0), columnspan=3, sticky="w")
     pie1.grid_remove()
     pie2.grid_remove()
     pie3.grid_remove()
@@ -44,9 +52,10 @@ def pointcharts():
 
 CTkLabel(root, text="imGraphs", text_color="#C0256F", font=("Kaushan Script", 64)).grid(row=0, column=2, columnspan=2, pady=(0, 50))
 CTkButton(root, text="Kołowy", anchor="center", text_color="white", fg_color="#C0256F", corner_radius=150,
-          font=("Quicksand", 20), command=piecharts).grid(row=1, column=0,columnspan=2, padx=(225, 0), pady=(0,50))
+          font=("Quicksand", 20), command=piecharts).grid(row=1, column=0, columnspan=2, padx=(225, 0), pady=(0,50))
 CTkButton(root, text="Słupkowy", anchor="center", text_color="white", fg_color="#C0256F", corner_radius=150,
-          font=("Quicksand", 20), command=barcharts).grid(row=1, column=2,columnspan=2, pady=(0,50))
+          font=("Quicksand", 20), command=barcharts).grid(row=1, column=2, columnspan=2, pady=(0,50))
 CTkButton(root, text="Punktowy", anchor="center", text_color="white", fg_color="#C0256F", corner_radius=150,
-          font=("Quicksand", 20), command=pointcharts).grid(row=1, column=4,columnspan=2, padx=(0, 225), pady=(0,50))
+          font=("Quicksand", 20), command=pointcharts).grid(row=1, column=4, columnspan=2, padx=(0, 225), pady=(0,50))
+
 root.mainloop()
